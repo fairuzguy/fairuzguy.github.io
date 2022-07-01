7d0dd63cdbf793c7c31bd28c05340dec63426151
Mohamed Elsayed <flylikethewind@FlyLikeTheWind.local>
2022-06-30 22:00:51 -0400

    Added some important files

---
+++ app.js
@@ -0,0 +1,15 @@
+const HTTP = require("http")
+const HOST = '0.0.0.0'
+//const DATA = JSON.stringify(require("./data.json"))
+const PORT = process.env.PORT || 8000
+
+const REQUEST_LISTENER = function(request, response) {
+    response.setHeader("Content-Type", "application/json")
+    response.writeHead(200) // HTTP status code 'OK'
+    response.end("Hello")
+}
+
+const SERVER = HTTP.createServer(REQUEST_LISTENER)
+SERVER.listen(PORT, HOST, function() {
+    console.log(`Server is running on http://${HOST}:${PORT}`)
+})
